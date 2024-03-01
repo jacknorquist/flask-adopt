e0c269f7bba74100a2a403c831c9e58fd81dd3cc
Jack Norquist <jack.norquist@outlook.com>
2024-03-01 08:52:47 -0600

    Added Pet class and AddPetForm

--- forms.py
+++ forms.py
@@ -1,1 +1,22 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm, FileAllowed
+from wtforms import StringField, FloatField, FileField, IntegerField, SelectField
+from wtforms.validators import InputRequired
+
+
+
+class AddPetForm(FlaskForm):
+    """Pet."""
+
+    name = StringField("Name", validators=[InputRequired()] )
+
+    species =StringField("Species", validators=[InputRequired()] )
+    photo_url = FileField("photo", validators=[FileAllowed(['jpg','jpeg','png'])])
+
+
+    age = IntegerField("Age", validators=[InputRequired()] )
+
+    notes = StringField("Notes")
+    available = SelectField('available',
+                            choices=[(True, "Yes"),
+                                      (False,"No")])
