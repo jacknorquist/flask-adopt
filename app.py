d8c42a0d61fdfdc917fb4ec75654620992ab3382
Jazz Cheema <jaskamalc@gmail.com>
2024-03-01 15:37:56 -0800

    worked up to step 5, add validation

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import connect_db, Pet
+from models import connect_db, Pet, db
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -13,6 +14,8 @@ app.config['SECRET_KEY'] = "secret"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", "postgresql:///adopt")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 
@@ -25,5 +28,41 @@ toolbar = DebugToolbarExtension(app)
 
 @app.get('/')
 def show_homepage():
+    """index home page."""
     pets = Pet.query.all()
     return render_template("homepage", pets=pets)
+
+
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    """add pet info; handle adding."""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        available = form.available.data
+
+        # do stuff with data/insert to db
+
+        pet = Pet(name = name,
+                         species = species,
+                         photo_url=photo_url,
+                         age=age,
+                         notes=notes,
+                         available=available)
+
+        db.session.add(pet)
+        db.session.commit()
+
+        return redirect("/")
+
+    else:
+        return render_template(
+            "add-pet.html", form=form)
--- forms.py
+++ forms.py
@@ -1,21 +1,27 @@
 """Forms for adopt app."""
-from flask_wtf import FlaskForm, FileAllowed
+from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, FileField, IntegerField, SelectField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Optional, URL
 
 
 
 class AddPetForm(FlaskForm):
-    """Pet."""
+    """Pet model."""
 
     name = StringField("Name", validators=[InputRequired()] )
-
-    species =StringField("Species", validators=[InputRequired()] )
-    photo_url = FileField("photo", validators=[FileAllowed(['jpg','jpeg','png'])])
-
-
-    age = IntegerField("Age", validators=[InputRequired()] )
-
+    species =SelectField("Species",
+                         choices=[('cat', 'Cat'),
+                                  ('porcupine', 'Porcupine'),
+                                  ('dog', 'Dog'),],
+                                  validators=[InputRequired()])
+    photo_url = StringField("Photo", validators=[Optional(),
+                                                 URL()])
+    age = SelectField("Age",
+                      choices=[('baby', 'Baby'),
+                                  ('young', 'Young'),
+                                  ('adult', 'Adult'),
+                                  ('senior', 'Senior')],
+                      validators=[InputRequired()])
     notes = StringField("Notes")
     available = SelectField('available',
                             choices=[(True, "Yes"),
